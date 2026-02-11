d0bd43a3881c5ca7329a7b44907c039a52248861
nb2912 <nihalbasaniwal2912@gmail.com>
2026-02-12 00:35:10 +0530

    removed hosting from firebae

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',               // <--- CRITICAL: Tells Next.js to generate static HTML
-  images: { unoptimized: true },  // <--- Required for static export if you use <Image>
+  /* config options here */
 };
 
 export default nextConfig;
